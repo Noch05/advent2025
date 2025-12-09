1a81ccc3bcab628a4211b74e2eedc68992f49fc8
Noah Ochital <noahochital@icloud.com>
2025-12-09 12:23:50 +0100

    reading in data better

---
+++ day8/day8.r
@@ -0,0 +1,1 @@
+input <- readr::read_csv("day8/input.txt", col_names = FALSE)
--- day9/day9.r
+++ day9/day9.r
@@ -1,14 +1,11 @@
 # Prepping and Splitting Coordinates
-input <- readr::read_lines("day9/input.txt") |>
-  strsplit(",") |>
-  lapply(as.numeric)
-
-x_coords <- vapply(input, \(x) x[1], FUN.VALUE = numeric(1))
-y_coords <- vapply(input, \(x) x[2], FUN.VALUE = numeric(1))
+input <- readr::read_csv("day9/input.txt", col_names = FALSE)
 
 ## Part 1
 
-x_mat <- abs(outer(x_coords, x_coords, `-`)) + 1
-y_mat <- abs(outer(y_coords, y_coords, `-`)) + 1
+x_mat <- abs(outer(input$X1, input$X1, `-`)) + 1
+y_mat <- abs(outer(input$X2, input$X2, `-`)) + 1
 area_mat <- x_mat * y_mat
 part1 <- max(area_mat)
+
+## Part 2
