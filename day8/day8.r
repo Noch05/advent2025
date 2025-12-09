f13360ffe67a6fb53b2e83d457ed0e8d39b304d2
Noah Ochital <noahochital@icloud.com>
2025-12-09 12:44:06 +0100

    day8 part1 setup

--- day8/day8.r
+++ day8/day8.r
@@ -1,1 +1,8 @@
-input <- readr::read_csv("day8/input.txt", col_names = FALSE)
+input <- readr::read_csv("day8/input.txt", col_names = c("x", "y", "z"))
+
+
+## Part 1
+dist_mat <- as.matrix(dist(input, method = "euclidean"))
+idx <- which(lower.tri(dist_mat), arr.ind = TRUE)
+edges <- data.frame(i = idx[, 1], j = idx[, 2], d = dist_mat[idx]) |>
+  dplyr::arrange(d)
--- day9/day9.r
+++ day9/day9.r
@@ -3,9 +3,9 @@ input <- readr::read_csv("day9/input.txt", col_names = FALSE)
 
 ## Part 1
 
-x_mat <- abs(outer(input$X1, input$X1, `-`)) + 1
-y_mat <- abs(outer(input$X2, input$X2, `-`)) + 1
-area_mat <- x_mat * y_mat
+l_mat <- abs(outer(input$X1, input$X1, `-`)) + 1
+w_mat <- abs(outer(input$X2, input$X2, `-`)) + 1
+area_mat <- l_mat * w_mat
 part1 <- max(area_mat)
 
 ## Part 2
